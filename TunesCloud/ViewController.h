fab7d94cc1e24c7691e03faa76e5367648b3eb53
nishanthrs <nishanth.salinamakki@gmail.com>
2015-04-07 15:37:57 -0700

    Initial Commit

---
+++ TunesCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TunesCloud
+//
+//  Created by Nishanth Salinamakki on 4/7/15.
+//  Copyright (c) 2015 Nishanth Salinamakki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TunesCloud/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TunesCloud
+//
+//  Created by Nishanth Salinamakki on 4/7/15.
+//  Copyright (c) 2015 Nishanth Salinamakki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
